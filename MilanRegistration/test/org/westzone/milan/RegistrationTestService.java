01f5732d828373c4f493c101608da9af4aa14a33
Chirayu Shah <shahchirayu@hotmail.com>
2014-05-26 12:07:50 -0700

    Adding unit test

---
+++ MilanRegistration/test/org/westzone/milan/RegistrationTestService.java
@@ -0,0 +1,19 @@
+package org.westzone.milan;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class RegistrationTestService {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
